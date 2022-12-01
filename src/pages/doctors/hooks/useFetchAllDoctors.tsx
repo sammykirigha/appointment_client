ab498c06ca0b80a1e0ba85b538f51539c4095fd1
sammykirigha <dkirigha18@gmail.com>
2022-12-01 09:48:01 +0300

    created the folder structure for the app

---
+++ src/pages/doctors/hooks/useFetchAllDoctors.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+type Props = {}
+
+const useFetchAllDoctors = (props: Props) => {
+
+  return (
+    <p>useFetchAllDoctors</p>
+  );
+};
+
+export default useFetchAllDoctors
